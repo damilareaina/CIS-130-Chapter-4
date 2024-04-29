f3ee9df68bb2ec97ce971dd0ae694cc76998e452
Damilare Aina <162380969+damilareaina@users.noreply.github.com>
2024-04-29 11:33:19 -0400

    Add files via upload

---
+++ Chapter_4/Samples/Test/image.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById('myImage').src='Smiling Leo Perfect GIF.gif'.innerHTML = "I like this stuff!.";
+}
+<img id="myImage" src="Smiling Leo Perfect GIF.gif" style="width:100px"></img>
